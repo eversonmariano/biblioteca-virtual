5d8596be7c03c2ea3a0c5341dfca52b559ff45da
eversonmariano <mariano.computacao@gmail.com>
2024-09-23 12:32:58 -0300

    projeto criado

---
+++ biblioteca-virtual/src/test/java/com/everson/biblioteca/BibliotecaVirtualApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everson.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaVirtualApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
