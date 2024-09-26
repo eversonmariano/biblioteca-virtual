6cb2d0beb76bec6d73f85cd430ad915129db7bea
eversonmariano <mariano.computacao@gmail.com>
2024-09-25 22:04:02 -0300

    implementando controller da entidade book

--- biblioteca-virtual/src/main/java/com/everson/biblioteca/book/Book.java
+++ biblioteca-virtual/src/main/java/com/everson/biblioteca/book/Book.java
@@ -40,4 +40,13 @@ public class Book extends BaseEntity {
     @OneToMany(mappedBy = "book")
     private List<BookTransactionHistory> histories;
 
+    @Transient
+    public double getRate(){
+        if(feedbacks == null || feedbacks.isEmpty()) {
+            return 0;
+        }
+        var rate = this.feedbacks.stream().mapToDouble(Feedback::getNote).average().orElse(0.0);
+        return Math.round(rate * 10) / 10.0;
+    }
+
 }
